fb06b82d9b91ca64d50e82580d014c5ad34a1550
Monish Akram <monishakram123@gmail.com>
2019-06-25 23:09:12 +0530

    Add View for train Route

--- src/routes/railway.js
+++ src/routes/railway.js
@@ -21,10 +21,19 @@ router.get(routes.TRAIN_ROUTE, (req, res) => {
 
 router.post(routes.TRAIN_ROUTE, (req, res) => {
 	console.log(`Post Request for ${routes.TRAIN_ROUTE} Received.`);
-	var train_number = constants.pages.TRAIN_ROUTE.form_fields.FIELD_TRAIN_NUMBER
+	var train_number = constants.pages.TRAIN_ROUTE.form_fields.FIELD_TRAIN_NUMBER;
 	railway.get_train_route(req.body[train_number])
 		.then(result => {
-			res.send(result);
+			data = {
+				"app_data": constants.app_data,
+				"page_title": constants.pages.TRAIN_ROUTE.page_title,
+				"form_fields": {
+					"action_url": routes.RAILWAY_API + routes.TRAIN_ROUTE,
+					...constants.pages.TRAIN_ROUTE.form_fields
+				},
+				"results": result
+			};
+			res.render("train_route", data);
 		})
 		.catch(error => {
 			console.log(error);
